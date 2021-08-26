e6dc6814ac91afc3ddad54399b9f65732791084f
Thomazl <luan.abrantes1999@gmail.com>
2021-08-26 13:03:51 -0300

    style

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function dots() {
 let skull = null;
 const loader = new THREE.OBJLoader();
 loader.load(
-  "brain-3d.obj",
+  "Cerebro_3D.obj",
   (obj) => {
     skull = obj.children[0];
     dots();
